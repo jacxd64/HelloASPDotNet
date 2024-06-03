4e4ccc557fb81a5616719877e90dc27d62cb0660
jacxd64 <70988180+jacxd64@users.noreply.github.com>
2024-06-03 10:03:01 -0500

    blank asp.net project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloASPDotNet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
